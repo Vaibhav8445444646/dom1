74f88a095b877580a6eeed13d28207694fb97901
Vaibhav Bisht <vabbisht219@gmail.com>
2024-01-12 22:20:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ const div = document.getElementById("root");
+ div.style.width = "300px";
+ div.style.height = "200px";
+ div.style.backgroundColor = "yellow";
+ div.style.margin = "20px";
+ div.style.padding = "10px";
+ div.style.fontSize = "18px";
+ div.style.fontWeight = "bold";
